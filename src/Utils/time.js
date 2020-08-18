65eaac2fbfe832f017979c718c4cf29edd6141d5
memowii <memogl25@gmail.com>
2020-08-18 18:21:59 -0500

    Add class Time; Add logic for buttoms

---
+++ src/Utils/time.js
@@ -0,0 +1,23 @@
+export class Time {
+  static getTime(timeInMilliseconds) {
+    let time = timeInMilliseconds;
+
+    const hours = this.formatUnitOfTime(Math.floor(time / (60 * 60 * 1000)));
+
+    time = time % (60 * 60 * 1000);
+    const minutes = this.formatUnitOfTime(Math.floor(time / (60 * 1000)));
+
+    time = time % (60 * 1000);
+    const seconds = this.formatUnitOfTime(Math.floor(time / 1000));
+
+    const milliseconds = this.formatUnitOfTime(time % 1000);
+
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+  }
+
+  static formatUnitOfTime(unitOfTime) {
+    return unitOfTime < 10
+      ? `0${unitOfTime}`.substring(0, 2)
+      : unitOfTime.toString().substring(0, 2);
+  }
+}
--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faSync } from "@fortawesome/free-solid-svg-icons";
 
+import { Time } from '../../Utils/time';
 import "./index.css";
 
 export function Timer() {
+  const [timerInput, setTimerInput] = useState(0);
+  const [calcTime, setCalcTime] = useState('00:00:00:00')
+
+  const onSecondsChanged = (value) => {
+    const seconds = parseInt(value);
+    if (seconds && typeof seconds === "number") {
+      setTimerInput(seconds);
+      setCalcTime(Time.getTime(seconds))
+    } else {
+      setTimerInput(0);
+    }
+  };
+
   return (
     <div className="Timer">
-      <div className="Timer__text-info">00:00:00:00</div>
+      <div className="Timer__text-info">{calcTime}</div>
 
-      <input className="Timer__input" maxLength="6" placeholder="0" />
+      <input
+        className="Timer__input"
+        maxLength="6"
+        placeholder="0"
+        value={timerInput}
+        onChange={(e) => onSecondsChanged(e.target.value)}
+      />
 
       <div className="Timer__panel">
         <div className="btn-group">
-          <button className="btn btn-lg btn-success" disabled="">
+          <button className="btn btn-lg btn-success" disabled={!(timerInput > 0)}>
             <div className="text-right">
               <FontAwesomeIcon icon={faPlay} size="2x" className="s" />
             </div>
           </button>
-          
+
           <button className="btn btn-lg btn-primary">
             <div className="text-left">
               <FontAwesomeIcon icon={faSync} size="2x" className="s" />
