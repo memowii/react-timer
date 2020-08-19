266bf07533063f46bcdbfaaa3dea2dffaa2e0818
memowii <memogl25@gmail.com>
2020-08-19 18:39:55 -0500

    Refactor Timer component

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -6,33 +6,55 @@ import { Time } from "../../Utils/time";
 import "./index.css";
 
 export function Timer() {
-  const [timerInput, setTimerInput] = useState(0);
-  const [calcTime, setCalcTime] = useState(Time.getTime(0));
+  const [milliseconds, setMilliseconds] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [timeInfo, setTimeInfo] = useState(Time.getTime(0));
   const [isTimerStarted, setIsTimerStarted] = useState(false);
+  const [timerInterval, setTimerInterval] = useState(null);
 
   const onSecondsChanged = (value) => {
-    const seconds = parseInt(value);
-    if (seconds && typeof seconds === "number") {
-      setTimes(seconds);
+    const enteredSeconds = parseInt(value);
+    if (enteredSeconds && typeof enteredSeconds === "number") {
+      const milliseconds = enteredSeconds * 1000;
+      setMilliseconds(milliseconds);
+      setTimeInfo(Time.getTime(milliseconds));
+      setSeconds(enteredSeconds);
     } else {
-      setTimes(0);
+      setMilliseconds(0);
+      setTimeInfo(Time.getTime(0));
+      setSeconds(0);
     }
   };
 
   const setTimes = (time) => {
-    setTimerInput(time);
-    setCalcTime(Time.getTime(time * 1000));
+    setMilliseconds(time);
+    setTimeInfo(Time.getTime(time / 1000));
   };
 
   const resetTimer = () => {
     setTimes(0);
     setIsTimerStarted(false);
-  }
+  };
+
+  const startTimer = () => {
+    setTimerInterval(
+      setInterval(() => {
+        console.log("do");
+        setMilliseconds((state) => {
+          console.log("do 2");
+          return state - 10;
+        });
+        setTimeInfo(Time.getTime(milliseconds));
+      }, 10)
+    );
+  };
+
+  const stopTimer = () => {};
 
   return (
     <div className="Timer">
       <div className="Timer__text-info" hidden={isTimerStarted}>
-        {calcTime}
+        {timeInfo}
       </div>
 
       {!isTimerStarted ? (
@@ -40,11 +62,11 @@ export function Timer() {
           className="Timer__input"
           maxLength="6"
           placeholder="0"
-          value={timerInput}
+          value={seconds}
           onChange={(e) => onSecondsChanged(e.target.value)}
         />
       ) : (
-        <div className="Timer__text-info Timer__text-info--big">{calcTime}</div>
+        <div className="Timer__text-info Timer__text-info--big">{timeInfo}</div>
       )}
 
       <div className="Timer__panel">
@@ -53,8 +75,15 @@ export function Timer() {
             className={`btn btn-lg btn-success ${
               isTimerStarted ? "btn-dark" : ""
             }`}
-            disabled={!(timerInput > 0)}
-            onClick={() => setIsTimerStarted((state) => !state)}
+            disabled={!(milliseconds > 0)}
+            onClick={() => {
+              if (!isTimerStarted) {
+                setIsTimerStarted(true);
+                startTimer();
+              } else {
+                setIsTimerStarted(false);
+              }
+            }}
           >
             <div className="text-right">
               {!isTimerStarted ? (
