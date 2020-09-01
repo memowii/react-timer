cfe95c8079265b40b48e9e474b3a4df439d2d795
memowii <memogl25@gmail.com>
2020-08-31 19:53:45 -0500

    Refactor Timer component to use a reducer hook

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,103 +1,84 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faSync, faStop } from "@fortawesome/free-solid-svg-icons";
 
-import { Time } from "../../Utils/time";
 import "./index.css";
 
+import { TimerInitialState, TimerReducer } from "../../reducers/timerReducer";
+
 export function Timer() {
-  const [milliseconds, setMilliseconds] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  const [timeInfo, setTimeInfo] = useState(Time.getTime(0));
-  const [isTimerStarted, setIsTimerStarted] = useState(false);
-  const [timerInterval, setTimerInterval] = useState(null);
-  const [isAlmostFinished, setIsAlmostFinished] = useState(false);
+  const [state, dispatch] = useReducer(TimerReducer, TimerInitialState);
 
   const onSecondsChanged = (value) => {
     const enteredSeconds = parseInt(value);
     if (enteredSeconds && typeof enteredSeconds === "number") {
       const enteredMilliseconds = enteredSeconds * 1000;
-      setTimes(enteredMilliseconds, enteredSeconds)
+      setTimes(enteredMilliseconds, enteredSeconds);
     } else {
-      setTimes(0, 0)
+      setTimes(0, 0);
     }
   };
 
   const setTimes = (milliseconds, seconds) => {
-    setMilliseconds(milliseconds);
-    setTimeInfo(Time.getTime(milliseconds));
-    setSeconds(seconds);
-  };
-
-  const resetTimer = () => {
-    setMilliseconds(0);
-    setSeconds(0);
-    setIsTimerStarted(false);
-    setTimeInfo(Time.getTime(0));
-    setIsAlmostFinished(false);
-    clearInterval(timerInterval);
+    dispatch({ type: "MILLISECONDS", payload: milliseconds });
+    dispatch({ type: "TIMEINFO", payload: milliseconds });
+    dispatch({ type: "SECONDS", payload: seconds });
   };
 
   const startTimer = () => {
-    setIsTimerStarted(true);
-    setTimerInterval(
-      setInterval(() => {
-        setMilliseconds((prevMilliseconds) => prevMilliseconds - 10);
-      }, 10)
-    );
+    dispatch({ type: "TIMER_STARTED", payload: true });
+
+    const timerInterval = setInterval(() => {
+      dispatch({ type: "DEC_MILLISECONDS" });
+      dispatch({ type: "UPDATE_TIMEINFO" });
+    }, 10);
+
+    dispatch({ type: "TIMER_INTERVAL", payload: timerInterval });
   };
 
   const stopTimer = () => {
-    setMilliseconds((mil) => {
-      setSeconds(Math.floor(mil / 1000));
-      setTimeInfo(Time.getTime(mil));
-      clearInterval(timerInterval);
-
-      return mil;
+    dispatch({
+      type: "SECONDS",
+      payload: Math.floor(state.milliseconds / 1000),
     });
-    setIsTimerStarted(false);
-    setIsAlmostFinished(false);
+    dispatch({ type: "TIMEINFO", payload: state.milliseconds });
+    clearInterval(state.timerInterval);
+    dispatch({ type: "TIMER_INTERVAL", payload: null });
+    dispatch({ type: "TIMER_STARTED", payload: false });
+    dispatch({ type: "TIMER_ALMOST_FINISHED", payload: false });
   };
 
-  useEffect(() => {
-    setTimeInfo(Time.getTime(milliseconds));
-
-    if (isTimerStarted && milliseconds < 10 * 1000) {
-      setIsAlmostFinished(true);
-    }
-
-    if (milliseconds < 0) {
-      setMilliseconds(0);
-      setSeconds(0);
-      setIsTimerStarted(false);
-      setTimeInfo(Time.getTime(0));
-      clearInterval(timerInterval);
-    }
-  }, [milliseconds]);
-
-  useEffect(() => () => clearInterval(timerInterval), [timerInterval]);
+  const resetTimer = () => {
+    dispatch({ type: "TIMER_STARTED", payload: false });
+    dispatch({ type: "MILLISECONDS", payload: 0 });
+    dispatch({ type: "SECONDS", payload: 0 });
+    dispatch({ type: "TIMEINFO", payload: 0 });
+    dispatch({ type: "TIMER_ALMOST_FINISHED", payload: false });
+    clearInterval(state.timerInterval);
+    dispatch({ type: "TIMER_INTERVAL", payload: null });
+  };
 
   return (
     <div className="Timer">
-      <div className="Timer__text-info" hidden={isTimerStarted}>
-        {timeInfo}
+      <div className="Timer__text-info" hidden={state.isTimerStarted}>
+        {state.timeInfo}
       </div>
 
-      {!isTimerStarted ? (
+      {!state.isTimerStarted ? (
         <input
           className="Timer__input"
           maxLength="6"
           placeholder="0"
-          value={seconds}
+          value={state.seconds}
           onChange={(e) => onSecondsChanged(e.target.value)}
         />
       ) : (
         <div
           className={`Timer__text-info Timer__text-info--big ${
-            isAlmostFinished ? "Timer__text-info--red" : ""
+            state.isAlmostFinished ? "Timer__text-info--red" : ""
           }`}
         >
-          {timeInfo}
+          {state.timeInfo}
         </div>
       )}
 
@@ -105,11 +86,11 @@ export function Timer() {
         <div className="btn-group">
           <button
             className={`btn btn-lg btn-success ${
-              isTimerStarted ? "btn-dark" : ""
+              state.isTimerStarted ? "btn-dark" : ""
             }`}
-            disabled={!(milliseconds > 0)}
+            disabled={!(state.milliseconds > 0)}
             onClick={() => {
-              if (!isTimerStarted) {
+              if (!state.isTimerStarted) {
                 startTimer();
               } else {
                 stopTimer();
@@ -117,7 +98,7 @@ export function Timer() {
             }}
           >
             <div className="text-right">
-              {!isTimerStarted ? (
+              {!state.isTimerStarted ? (
                 <FontAwesomeIcon icon={faPlay} size="2x" className="s" />
               ) : (
                 <FontAwesomeIcon icon={faStop} size="2x" className="s" />
